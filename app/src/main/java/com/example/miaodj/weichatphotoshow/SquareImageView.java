5c83c98f9019d794da05b6f1f83e0a1b47954a0f
Mettre <1052461392@qq.com>
2017-02-10 15:06:52 +0800

    第一次提交

---
+++ app/src/main/java/com/example/miaodj/weichatphotoshow/SquareImageView.java
@@ -0,0 +1,25 @@
+package com.example.miaodj.weichatphotoshow;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * 方图
+ */
+public class SquareImageView extends ImageView {
+
+    public SquareImageView(Context context) {
+        this(context, null);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        setMeasuredDimension(widthSize, widthSize);
+    }
+}
